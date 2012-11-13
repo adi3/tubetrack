e2f22470c8c2a1693b658eba5d17cae09d961bfd
Aditya Singh <adisin8@gmail.com>
2012-11-13 15:19:50 +0530

    Updated meta tag for icon reference.

--- application/views/elements/track_header.php
+++ application/views/elements/track_header.php
@@ -7,7 +7,8 @@
     <?= link_tag("css/960.css") ?>
     <?= link_tag("css/reset.css") ?>
     <?= link_tag("resources/icon_x.ico", "shortcut icon", "image/ico") ?>
-    
+	
+    <meta property="og:image" content="<?= site_url('resources/icon_x.png') ?>" />
     <meta name="robots" content="no-cache" />
     <meta name="description" content="One-click stats tracking for YouTube videos. TubeTrack is a tool for quick and easy
                                                             tracking, sorting and comparing of YouTube videos in real-time." />
